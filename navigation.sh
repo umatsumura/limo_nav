a5395b6cce742130c3f6e3025f80b7a9a47561df
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-06-27 11:53:39 +0900

    added some files

--- navigation.sh
+++ navigation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gnome-terminal --tab -t "main-1" -- bash -c "source ~/.bashrc; roslaunch limo_bringup limo_start.launch; exec bash"
-sleep 3
-gnome-terminal --tab -t "main-1" -- bash -c "source ~/.bashrc; roslaunch limo_nav navigation.launch; exec bash"
+gnome-terminal  "limo-start" -- bash -c "source ~/.bashrc; roslaunch limo_bringup limo_start.launch; exec bash"
+sleep 5
+gnome-terminal "limo-navigation" -- bash -c "source ~/.bashrc; roslaunch limo_nav navigation.launch; exec bash"

f2ffd3ac9763978226a4c2d6d610e224c7ff71be
coke-official <cokeofficial1886@gmail.com>
2022-08-03 12:46:20 +0800

    updated the initial location

--- navigation.sh
+++ navigation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-gnome-terminal  "limo-start" -- bash -c "source ~/.bashrc; roslaunch limo_bringup limo_start.launch; exec bash"
-sleep 5
-gnome-terminal "limo-navigation" -- bash -c "source ~/.bashrc; roslaunch limo_nav navigation.launch; exec bash"
+gnome-terminal  "limo-start" -- bash -c "source ~/.bashrc; roslaunch limo_bringup limo_start.launch;"
+sleep 8
+gnome-terminal "limo-navigation" -- bash -c "source ~/.bashrc; roslaunch limo_nav navigation.launch;"
+sleep 2
+gnome-terminal "limo-navigation" -- bash -c "source ~/.bashrc; roslaunch limo_nav battery.launch;"
+
